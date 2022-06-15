8d2c7353aaf68b9ac764f7045e2c2ac316160269
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-06-15 21:25:18 +0700

    update map selection

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -19,6 +19,7 @@ const center = computed(() => {
   <GMapMap
     :center="center"
     :zoom="7"
+    ref="myMapRef"
     map-type-id="terrain"
     :options="{
       streetViewControl: false,
@@ -40,18 +41,21 @@ const center = computed(() => {
         v-for="(m, index) in markers"
         :position="m.position"
         :clickable="true"
+        ref="myMarkerRef"
         :draggable="false"
         :icon="{
           url: iconMap(
             m.data.Type,
             m.data.IndustryCategoryGroups?.length > 0 &&
-              m.data.IndustryCategoryGroups[0]
+              m.data.IndustryCategoryGroups[0],
+            null
           ),
           scaledSize: { width: 55, height: 75 },
         }"
         @click="
           storeMap.selectMarker(m);
           storeMap.openSidebar();
+          checkObject(markers, m);
           showByIndex = index;
         "
         @mouseover="showByIndex = index"
@@ -86,44 +90,138 @@ export default {
       markers: [],
       showByIndex: null,
       iconPoi: "/images/poi.png",
+      iconPoiSelected: "/images/poi-selected.png",
       iconExp: "/images/experience.png",
       iconActiv: "/images/activity.png",
       iconRestaurant: "/images/restaurant.png",
       iconShop: "/images/shopping.png",
       iconAccomm: "/images/accommodation.png",
+      iconExpSelected: "/images/experience-selected.png",
+      iconActivSelected: "/images/activity-selected.png",
+      iconRestaurantSelected: "/images/restaurant-selected.png",
+      iconShopSelected: "/images/shopping-selected.png",
+      iconAccommSelected: "/images/accommodation-selected.png",
       path: [],
       pathData: [],
     };
   },
   methods: {
-    iconMap(type, group) {
+    checkObject(markers, selected) {
+      debugger;
+      this.$refs.myMarkerRef.map((item) => {
+        markers.map((m) => {
+          if (
+            item.position.lat === m.position.lat &&
+            item.position.lng === m.position.lng
+          ) {
+            item.$markerPromise.then((marker) => {
+              const iconProps = {
+                url: this.iconMap(
+                  m.data.Type,
+                  m.data.IndustryCategoryGroups?.length > 0 &&
+                    m.data.IndustryCategoryGroups[0],
+                  null
+                ),
+                scaledSize: { width: 55, height: 75 },
+              };
+              marker.setIcon(iconProps);
+            });
+          }
+        });
+        if (
+          item.position.lat === selected.position.lat &&
+          item.position.lng === selected.position.lng
+        ) {
+          item.$markerPromise.then((marker) => {
+            const iconProps = {
+              url: this.iconMap(
+                selected.data.Type,
+                selected.data.IndustryCategoryGroups?.length > 0 &&
+                  selected.data.IndustryCategoryGroups[0],
+                true
+              ),
+              scaledSize: { width: 55, height: 75 },
+            };
+            marker.setIcon(iconProps);
+          });
+        }
+      });
+    },
+    iconMap(type, group, selected) {
       let icon = this.iconPoi;
       switch (type) {
         case 6:
-          icon = this.iconPoi;
+          switch (selected) {
+            case true:
+              icon = this.iconPoiSelected;
+              break;
+            case null:
+              icon = this.iconPoi;
+              break;
+          }
           break;
         case 5:
-          icon = this.iconExp;
+          switch (selected) {
+            case true:
+              icon = this.iconExpSelected;
+              break;
+            case null:
+              icon = this.iconExp;
+              break;
+          }
           break;
         case 3:
           switch (group) {
             case 0:
-              icon = this.iconAccomm;
+              switch (selected) {
+                case true:
+                  icon = this.iconAccommSelected;
+                  break;
+                case null:
+                  icon = this.iconAccomm;
+                  break;
+              }
               break;
             case 1:
-              icon = this.iconActiv;
+              switch (selected) {
+                case true:
+                  icon = this.iconActivSelected;
+                  break;
+                case null:
+                  icon = this.iconActiv;
+                  break;
+              }
               break;
             case 2:
-              icon = this.iconRestaurant;
+              switch (selected) {
+                case true:
+                  icon = this.iconRestaurantSelected;
+                  break;
+                case null:
+                  icon = this.iconRestaurant;
+                  break;
+              }
               break;
             case 3:
-              icon = this.iconShop;
+              switch (selected) {
+                case true:
+                  icon = this.iconShopSelected;
+                  break;
+                case null:
+                  icon = this.iconShop;
+                  break;
+              }
               break;
           }
       }
 
       return icon;
     },
+  },
+  mounted() {
+    this.$refs.myMapRef.$mapPromise.then((mapObject) => {
+      console.log("map is loaded now", window.google);
+    });
   },
   components: { MapDetail },
 };
@@ -157,6 +255,20 @@ img[src="/images/accommodation.png"],
 img[src="/images/restaurant.png"],
 img[src="/images/shopping.png"] {
   filter: drop-shadow(0 0 6px black);
+  width: 35px !important;
+  height: 45px !important;
+  left: 50% !important;
+  top: 50% !important;
+  transform: translate(-50%, -50%);
+}
+
+img[src="/images/poi-selected.png"],
+img[src="/images/experience-selected.png"],
+img[src="/images/activity-selected.png"],
+img[src="/images/accommodation-selected.png"],
+img[src="/images/restaurant-selected.png"],
+img[src="/images/shopping-selected.png"] {
+  filter: drop-shadow(0 0 6px red);
   width: 35px !important;
   height: 45px !important;
   left: 50% !important;
